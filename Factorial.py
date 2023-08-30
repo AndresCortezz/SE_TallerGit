88fb7a589a5ae1d39d4fed39e31797173c6066e0
AndresCortezz <andrescortez916@gmail.com>
2023-08-29 22:23:57 -0500

    factorial que si compila2.0

---
+++ Factorial.py
@@ -0,0 +1,11 @@
+numero = int(input("Digite un numero: "))
+ 
+factorial = 1
+
+for i in range(1,numero+1):
+    factorial= factorial * i
+
+
+print("El factorial del numero ingresado es: ",factorial)
+
+
